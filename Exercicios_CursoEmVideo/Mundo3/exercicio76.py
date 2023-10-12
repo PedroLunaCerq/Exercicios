92b8b6ce58cf829e0a679be61850240e4910fa19
PedroHenriqueCerq <lunissnyan11@gmail.com>
2023-10-12 10:29:28 -0300

    Exercícios da aula sobre tuplas concluídos.

--- Exercicios_CursoEmVideo/Mundo3/exercicio76.py
+++ Exercicios_CursoEmVideo/Mundo3/exercicio76.py
@@ -1,14 +1,11 @@
-#crie uma tupla única com uma lista de produtos e seus respectivos preços, na sequência. No final,
-#mostre os preços acompanhado de seus produtos, em forma tabular.
+produtos = ("Lápis", 1.75, "Borracha", 2.00, "Caderno", 15.90, "Estojo", 25.00, "Transferidor", 4.20,
+            "Compasso", 9.99, "Mochila", 120.32, "Canetas", 22.30, "Livro", 34.90)
 
+print('=' * 50)
+print('{:^50}'.format('LISTAGEM DE PREÇOS'))
+print('=' * 50)
 
-produtos = []
-preços = []
-tupla = ('Lápis', 1.75, 'Borracha', 2.00, 'Caderno', 15.90)
-produtos.append(tupla[0:len(tupla):2])
-preços.append(tupla[1:len(tupla):2])
+for c in range(0, len(produtos), 2):
+    print(f'{produtos[c]:.<40}', f'R${produtos[c+1]:>7.2f}')
 
-print('=' * 15)
-for produto in produtos:
-    print(produto)
-print('=' * 15)
+print('=' * 50)
--- Exercicios_CursoEmVideo/Mundo3/exercicio77.py
+++ Exercicios_CursoEmVideo/Mundo3/exercicio77.py
@@ -1,1 +1,8 @@
 #Crie um programa com uma tupla com várias palavras. Depois disso, deve mostrar para cada palavra, as suas vogais.
+
+tupla = ('Monitor', 'Mouse', 'Celular', 'Computador', 'Controle')
+for item in tupla:
+    print(f'\nNa palavra {item} temos as vogais ', end = '')
+    for letra in item: 
+        if letra.upper() in 'AEIOU':
+            print(letra.upper(), end = '')
