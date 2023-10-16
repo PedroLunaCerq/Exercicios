772cff6e82d9e04aeefb7a8168cc9b5ab5192608
PedroHenriqueCerq <lunissnyan11@gmail.com>
2023-10-16 19:48:34 -0300

    Início dos exercícios da segunda parte. Em breve, dicionários.

---
+++ Exercicios_CursoEmVideo/Mundo3/exercicio84.py
@@ -0,0 +1,31 @@
+temp = []
+princ = []
+mai = men = 0
+
+while True:
+    temp.append(input('Insira o nome: '))
+    temp.append(float(input('Insira o peso: ')))
+    if len(princ) == 0:
+        mai = men = temp[1]
+    else:
+        if temp[1] > mai:
+            mai = temp[1]
+        if temp[1] < men:
+            men = temp[1]
+    princ.append(temp[:])
+    temp.clear()
+    resp = input('Quer continuar? [S/N] ').upper()
+    if resp == 'N':
+        break
+
+print(f'Ao todo, você cadastrou {len(princ)} pessoas.')
+print(f'O maior peso foi de {mai} kg. Peso de ', end = '')
+for p in princ:
+    if p[1] == mai:
+        print(f'[{p[0]}] ', end = '')
+print()
+print(f'O menor peso foi de {men} kg.', end = '')
+for p in princ:
+    if p[1] == men:
+        print(f'[{p[0]}] ', end = '')
+print()
