6fbebc8cba75899a9e7ee5393ba5f9ea2f644d19
PedroHenriqueCerq <lunissnyan11@gmail.com>
2023-10-20 15:24:23 -0300

    Exercício 87.py

---
+++ Exercicios_CursoEmVideo/Mundo3/exercicio88.py
@@ -0,0 +1,14 @@
+import random
+jogo = []
+jogos = []
+
+quantidade = int(input('Quantos jogos você irá fazer? '))
+for i in range(quantidade):
+    for c in range(0, 6):
+        n = random.randrange(1, 60)
+        jogo.append(n)
+    jogos.append(jogo)
+    jogo = []
+
+print('Você pode fazer os seguintes jogos:')
+print(jogos)
