30868253a67fb70f38e8d2d8ab03214ceb8c7312
PedroHenriqueCerq <lunissnyan11@gmail.com>
2023-10-20 15:17:38 -0300

    Exercício 87 (upgrade do 86)

--- Exercicios_CursoEmVideo/Mundo3/exercicio86.py
+++ Exercicios_CursoEmVideo/Mundo3/exercicio86.py
@@ -2,16 +2,28 @@ matriz1 = []
 matriz2 = []
 matriz3 = []
 matriz_total = []
+soma = 0
+soma_terceira = 0
+maior_valor = 0
 
 for i in range(0, 3):
     n = int(input('Insira um valor para a primeira coluna da matriz: '))
     matriz1.append(n)
+    if n % 2 == 0:
+        soma += n
 for i in range(0, 3):
     n = int(input('Insira um valor para a segunda coluna da matriz: '))
     matriz2.append(n)
+    if n % 2 == 0:
+        soma += n
+    if n >= maior_valor:
+        maior_valor = n
 for i in range(0, 3):
     n = int(input('Insira um valor para a terceira coluna da matriz: '))
     matriz3.append(n)
+    if n % 2 == 0:
+        soma += n
+    soma_terceira += n
 
 matriz_total.append(matriz1)
 matriz_total.append(matriz2)
@@ -20,4 +32,8 @@ matriz_total.append(matriz3)
 print('=' * 15)
 for c in matriz_total:
     print(c)
+print('-' * 10)
+print(f'A soma de todos os valores pares é {soma}.')
+print(f'A soma dos valores da terceira coluna é {soma_terceira}.')
+print(f'O maior valor da segunda linha é {maior_valor}.')
 print('=' * 15)
