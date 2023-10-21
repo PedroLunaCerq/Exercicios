93e1f8f44742f90c5b8b0c2fb6cb66ea2ed9697d
PedroHenriqueCerq <lunissnyan11@gmail.com>
2023-10-21 15:02:49 -0300

    Exercícios de listas finalizados. 89 Feito com o método guanabara xD

--- Exercicios_CursoEmVideo/Mundo3/exercicio89.py
+++ Exercicios_CursoEmVideo/Mundo3/exercicio89.py
@@ -1,19 +1,25 @@
-alunos = []
-notas = []
-medias = []
+ficha = []
 
 while True:
     nome = input('Insira o nome: ')
-    alunos.append(nome)
-    nota_individual = []
-    n1 = int(input('Insira a primeira nota: '))
-    nota_individual.append(n1)
-    n2 = int(input('Insira a segunda nota: '))
-    nota_individual.append(n2)
-    operar = input('Deseja continuar? [S/N]').upper()
-    if operar == 'N':
+    n1 = float(input('Insira a primeira nota: '))
+    n2 = float(input('Insira a segunda nota: '))
+    media = (n1 + 2) / 2
+    ficha.append([nome, n1, n2, media])
+    resp = input('Quer continuar? [S/N] ').upper()
+    if resp == 'N':
         break
-
-for nota in notas:
-    media = (nota[1] + nota[2]) / 2
-    medias.append(media)
+print('=-' * 20)
+print(f'{"No.":4<}{"Nome":<10}{"Média":>8}')
+print('-' * 18)
+for i, a in enumerate(ficha):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+while True:
+    print('-' * 35)
+    opc = int(input('Mostrar notas de qual aluno? (999 Interrompe).  '))
+    if opc == 999:
+        print('Finalizando...')
+        break
+if opc <= len(ficha) - 1:
+    print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}')
+print('<<< Volte Sempre! >>>')
