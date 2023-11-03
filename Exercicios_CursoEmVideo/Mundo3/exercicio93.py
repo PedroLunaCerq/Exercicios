ccbe932b1ae95b8e4badebc51ae7d2d95cffc2a2
PedroHenriqueCerq <lunissnyan11@gmail.com>
2023-11-02 21:15:19 -0300

    Desafio 95: Upgrade do 93

--- Exercicios_CursoEmVideo/Mundo3/exercicio93.py
+++ Exercicios_CursoEmVideo/Mundo3/exercicio93.py
@@ -1,22 +1,31 @@
+jogadores = []
 jogador = {}
-gols = []
-total = 0
 
-jogador['nome'] = input('Insira o nome do jogador: ').title()
-partidas = int(input('Insira quantas partidas ele jogou no campeonato: '))
-jogador['partidas'] = partidas
-for c in range(partidas):
-    a = int(input(f'Quantos gols foram marcados na {c + 1}° partida? '))
-    gols.append(a)
-    total += a
-jogador['gols'] = gols[:]
-jogador['total'] = total
+while True:
+    gols = []
+    total = 0
+    jogador.clear()
+    jogador['nome'] = input('Insira o nome do jogador: ').title()
+    partidas = int(input('Insira quantas partidas ele jogou no campeonato: '))
+    jogador['partidas'] = partidas
+    for c in range(partidas):
+        a = int(input(f'Quantos gols foram marcados na {c + 1}° partida? '))
+        gols.append(a)
+        total += a
+    jogador['gols'] = gols[:]
+    jogador['total'] = total
+    jogadores.append(jogador.copy())
+    operar = input('Deseja continuar? [S/N] ')
+    if operar in 'Nn':
+        break
 
-print(f"O nome do jogador é {jogador['nome']}, jogou {jogador['partidas']} partidas e no total fez {jogador['total']} gols.")
 print('-' * 15)
-for c, gol in enumerate(jogador['gols']):
-    print(f'Na {c + 1}° partida fez {gol} gols.')
-print(f'Foi um total de {total} gols')
-print('-' * 15)
-print('<<Programa encerrado!>>')
+for c, jogador in enumerate(jogadores):
+    print(f"{c + 1}° - {jogadores[c]['nome']}")
 print()
+while True:
+    operar = int(input('Deseja ver as informações de qual jogador? [0 Cancela]: '))
+    if operar == 0:
+        break
+    print(jogadores[operar - 1])
+print('<<Programa Encerrado!>>')
