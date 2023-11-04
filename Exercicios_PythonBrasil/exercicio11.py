63ebcd72ddbbc434a04ef4fae2ab0a3fe9933d96
PedroHenriqueCerq <lunissnyan11@gmail.com>
2023-11-04 19:38:55 -0300

    Exercício 11: Python Brasil

---
+++ Exercicios_PythonBrasil/exercicio11.py
@@ -0,0 +1,11 @@
+n1 = int(input('Insira o primeiro número inteiro: '))
+n2 = int(input('Insira o segundo número inteiro: '))
+n3 = float(input('Agora, um número real: '))
+
+r1 = (n1 * 2) + (n2 / 2)
+r2 = (n1 * 3) + n3
+r3 = n3 ** 3
+
+print(r1)
+print(r2)
+print(r3)
